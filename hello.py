374b42953706c84d9afd5bc84fbfc6a78da34df5
Sylvain Gadrat <sgadrat@gmail.com>
2025-04-11 18:09:13 +0200

    wip: webservice

--- hello.py
+++ hello.py
@@ -1,11 +1,43 @@
-from flask import Flask
-from flask import request
-app = Flask(__name__)
+import flask
+import json
+import pathlib
+import subprocess
+app = flask.Flask(__name__)
+
+stb_path = pathlib.Path("/root/super-tilt-bro")
+mod_path = stb_path / "game-mod" / "mod.json"
+build_bin_path = stb_path / "build.sh"
+
+running = False
 
 @app.route("/build/char", methods=['POST'])
 def hello():
-    return request.json
+    return flask.request.json
 
 @app.route("/build/mod", methods=['POST'])
 def hello2():
-    return "Hello Universe!"
+	global running
+
+	if running:
+		return (400, {"returncode": None, "output": "busy right now"})
+
+	try:
+		running = True
+
+		with open(mod_path, "w") as mod_file:
+			json.dump(flask.request.json, mod_file)
+		result = subprocess.run(
+			[str(build_bin_path)],
+			text=True,
+			stdout=subprocess.PIPE,
+			stderr=subprocess.STDOUT
+		)
+		return (
+			200 if result.returncode == 0 else 400,
+			{
+				"returncode": result.returncode,
+				"output": result.stdout,
+			}
+		)
+	finally:
+		running = False
