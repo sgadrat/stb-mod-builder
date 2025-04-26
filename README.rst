e03929e60cccd25bd9b0d73c60336fe65d018f62
Sylvain Gadrat <sgadrat@wontfix.it>
2025-04-26 10:49:15 +0200

    webservice: functional /build/mod route

--- README.rst
+++ README.rst
@@ -10,4 +10,10 @@ Notes
 
 ::
 
+	# use "docker build --no-cache" to get the latest git revision of STB
 	docker build -t stb-mod-builder:latest .
+	docker run -p 127.0.0.1:8000:8000 stb-mod-builder:latest
+
+	./json_to_dict.py ~/workspace/nes/tilt/game-mod/mod.json > /tmp/stb25mod.json
+	curl -XPOST -vv http://127.0.0.1:8000/build/mod -d @/tmp/stb25mod.json -H 'Content-Type: application/json' > /tmp/out.json
+	cat /tmp/out.json | jq -r .roms.unrom | base64 -d > /tmp/tilt_unrom\(E\).nes
--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
+import base64
 import flask
 import json
+import logging
 import pathlib
 import subprocess
 app = flask.Flask(__name__)
 
+logging.basicConfig(level=logging.INFO)
+
 stb_path = pathlib.Path("/root/super-tilt-bro")
 mod_path = stb_path / "game-mod" / "mod.json"
 build_bin_path = stb_path / "build.sh"
+build_log_path = stb_path / "build.log"
+rom_unrom_path = stb_path / "tilt_no_network_unrom_(E).nes"
 
 running = False
 
@@ -19,7 +25,7 @@ def hello2():
 	global running
 
 	if running:
-		return (400, {"returncode": None, "output": "busy right now"})
+		return {"returncode": None, "output": "busy right now"}, 400
 
 	try:
 		running = True
@@ -32,12 +38,30 @@ def hello2():
 			stdout=subprocess.PIPE,
 			stderr=subprocess.STDOUT
 		)
+
+		def load_file(path, mode):
+			try:
+				with open(path, f"r{mode}") as log_file:
+					return log_file.read()
+			except Exception as e:
+				logging.error(f"Failed to open \"{path}\": {e}")
+				if mode == "t":
+					return ""
+				elif mode == "b":
+					return b""
+				else:
+					assert False
+
 		return (
-			200 if result.returncode == 0 else 400,
 			{
 				"returncode": result.returncode,
 				"output": result.stdout,
-			}
+				"build_log": load_file(build_log_path, "t"),
+				"roms": {
+					"unrom": base64.b64encode(load_file(rom_unrom_path, "b")).decode(),
+				}
+			},
+			200 if result.returncode == 0 else 400
 		)
 	finally:
 		running = False
